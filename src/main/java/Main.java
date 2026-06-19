d299cfea25f6d9e7c59bd0affab4366106a200c8
Pratham Surana <prathamsurana@Prathams-MacBook-Air.local>
2026-06-20 00:21:49 +0530

    implement stdout redirection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -6,8 +8,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
+        PrintStream originalOut = System.out;
 
         while (true) {
+            System.setOut(originalOut);
             System.out.print("$ ");
             System.out.flush();
 
@@ -21,15 +25,49 @@ public class Main {
                 continue;
             }
 
-            String command = parsedArgs.get(0);
+            // Extract redirection if present
+            String redirectFile = null;
+            int redirectIndex = -1;
+            for (int i = 0; i < parsedArgs.size(); i++) {
+                String arg = parsedArgs.get(i);
+                if (arg.equals(">") || arg.equals("1>")) {
+                    if (i + 1 < parsedArgs.size()) {
+                        redirectFile = parsedArgs.get(i + 1);
+                        redirectIndex = i;
+                        break;
+                    }
+                }
+            }
+
+            List<String> commandArgs = parsedArgs;
+            if (redirectIndex != -1) {
+                commandArgs = new ArrayList<>(parsedArgs.subList(0, redirectIndex));
+            }
+
+            if (commandArgs.isEmpty()) {
+                continue;
+            }
+
+            String command = commandArgs.get(0);
+
+            // Handle redirection setup for builtins
+            File outFile = null;
+            if (redirectFile != null) {
+                outFile = new File(redirectFile);
+                if (outFile.getParentFile() != null) {
+                    outFile.getParentFile().mkdirs();
+                }
+                PrintStream fileOut = new PrintStream(new FileOutputStream(outFile, false));
+                System.setOut(fileOut);
+            }
 
             if (command.equals("exit")) {
                 break;
             } else if (command.equals("echo")) {
                 StringBuilder sb = new StringBuilder();
-                for (int i = 1; i < parsedArgs.size(); i++) {
-                    sb.append(parsedArgs.get(i));
-                    if (i < parsedArgs.size() - 1) {
+                for (int i = 1; i < commandArgs.size(); i++) {
+                    sb.append(commandArgs.get(i));
+                    if (i < commandArgs.size() - 1) {
                         sb.append(" ");
                     }
                 }
@@ -37,7 +75,7 @@ public class Main {
             } else if (command.equals("pwd")) {
                 System.out.println(System.getProperty("user.dir"));
             } else if (command.equals("cd")) {
-                String path = parsedArgs.size() > 1 ? parsedArgs.get(1) : "~";
+                String path = commandArgs.size() > 1 ? commandArgs.get(1) : "~";
                 File dir;
                 
                 if (path.equals("~")) {
@@ -51,10 +89,11 @@ public class Main {
                 if (dir.exists() && dir.isDirectory()) {
                     System.setProperty("user.dir", dir.getCanonicalPath());
                 } else {
+                    System.setOut(originalOut);
                     System.out.println("cd: " + path + ": No such file or directory");
                 }
             } else if (command.equals("type")) {
-                String arg = parsedArgs.get(1);
+                String arg = commandArgs.get(1);
                 if (arg.equals("echo") || arg.equals("exit") || arg.equals("type") || arg.equals("pwd") || arg.equals("cd")) {
                     System.out.println(arg + " is a shell builtin");
                 } else {
@@ -68,11 +107,17 @@ public class Main {
             } else {
                 String executablePath = getPath(command);
                 if (executablePath != null) {
-                    ProcessBuilder pb = new ProcessBuilder(parsedArgs);
-                    pb.inheritIO();
+                    ProcessBuilder pb = new ProcessBuilder(commandArgs);
+                    if (outFile != null) {
+                        pb.redirectOutput(outFile);
+                        pb.redirectError(ProcessBuilder.Redirect.INHERIT);
+                    } else {
+                        pb.inheritIO();
+                    }
                     Process process = pb.start();
                     process.waitFor();
                 } else {
+                    System.setOut(originalOut);
                     System.out.println(command + ": command not found");
                 }
             }
@@ -91,7 +136,6 @@ public class Main {
 
             if (c == '\\' && !inSingleQuotes) {
                 if (inDoubleQuotes) {
-                    // Inside double quotes, only escape specific characters
                     if (i + 1 < input.length()) {
                         char nextChar = input.charAt(i + 1);
                         if (nextChar == '"' || nextChar == '\\' || nextChar == '$' || nextChar == '`') {
@@ -104,7 +148,6 @@ public class Main {
                         currentArg.append(c);
                     }
                 } else {
-                    // Outside quotes, always escape the next character
                     if (i + 1 < input.length()) {
                         currentArg.append(input.charAt(i + 1));
                         i++;
