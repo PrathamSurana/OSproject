f09ebef6421afcff327e9325cf23fd2637ca333e
Pratham Surana <prathamsurana@Prathams-MacBook-Air.local>
2026-06-19 16:10:50 +0530

    implement a REPL loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
-        System.out.flush();
-
-        // Read user input
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        
-        // Print the invalid command message
-        System.out.println(input + ": command not found");
+
+        while (true) {
+            System.out.print("$ ");
+            System.out.flush();
+
+            String input = scanner.nextLine();
+            
+            System.out.println(input + ": command not found");
+        }
     }
 }
