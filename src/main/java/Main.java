98bf93580fcac1a5a7b5db060e1643b8d8c4ab87
Pratham Surana <prathamsurana@Prathams-MacBook-Air.local>
2026-06-19 15:54:50 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // TODO: Uncomment the code below to pass the first stage
-        // System.out.print("$ ");
+        // Uncomment this block to pass the first stage
+        System.out.print("$ ");
+        System.out.flush();
+
+        // Wait for user input
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
     }
 }
